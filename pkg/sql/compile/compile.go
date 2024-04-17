5cd5bea64d932401a52cb0cf4ae51ba084cd70b4
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-04-17 16:53:31 -0700

    init

--- pkg/sql/compile/compile.go
+++ pkg/sql/compile/compile.go
@@ -1,12 +1,11 @@
 package compile
 
 import (
-	"fmt"
+	"github.com/gogo/protobuf/test/merge"
 	"mo_join/pkg/sql/colexec/connector"
 	"mo_join/pkg/sql/plan"
 	"mo_join/pkg/vm"
 	"mo_join/pkg/vm/mheap"
-	"mo_join/pkg/vm/overload"
 	"mo_join/pkg/vm/process"
 )
 
@@ -22,7 +21,7 @@ func (c *Compile) compileJoin(n *plan.Node, ss []*Scope, children []*Scope, join
 			chp.Proc = process.NewFromProc(mheap.New(c.proc.Mp.Gm), c.proc, len(children))
 			for j := range children {
 				children[j].Instructions = append(children[j].Instructions, vm.Instruction{
-					Op: overload.Connector,
+					Op: vm.Connector,
 					Arg: &connector.Argument{
 						Mmu: chp.Proc.Mp.Gm,
 						Reg: chp.Proc.Reg.MergeReceivers[j],
@@ -30,7 +29,7 @@ func (c *Compile) compileJoin(n *plan.Node, ss []*Scope, children []*Scope, join
 				})
 			}
 			chp.Instructions = append(chp.Instructions, vm.Instruction{
-				Op:  overload.Merge,
+				Op:  vm.Merge,
 				Arg: &merge.Argument{},
 			})
 		}
@@ -40,16 +39,14 @@ func (c *Compile) compileJoin(n *plan.Node, ss []*Scope, children []*Scope, join
 		}
 		rs[i].Proc = process.NewFromProc(mheap.New(c.proc.Mp.Gm), c.proc, 2)
 		ss[i].Instructions = append(ss[i].Instructions, vm.Instruction{
-			Op: overload.Connector,
+			Op: vm.Connector,
 			Arg: &connector.Argument{
-				Mmu: rs[i].Proc.Mp.Gm,
 				Reg: rs[i].Proc.Reg.MergeReceivers[0],
 			},
 		})
 		chp.Instructions = append(chp.Instructions, vm.Instruction{
-			Op: overload.Connector,
+			Op: vm.Connector,
 			Arg: &connector.Argument{
-				Mmu: rs[i].Proc.Mp.Gm,
 				Reg: rs[i].Proc.Reg.MergeReceivers[1],
 			},
 		})
@@ -59,41 +56,21 @@ func (c *Compile) compileJoin(n *plan.Node, ss []*Scope, children []*Scope, join
 		if len(n.OnList) == 0 {
 			for i := range rs {
 				rs[i].Instructions = append(rs[i].Instructions, vm.Instruction{
-					Op:  overload.Product,
+					Op:  vm.Product,
 					Arg: constructProduct(n, c.proc),
 				})
 			}
 		} else {
 			for i := range rs {
 				rs[i].Instructions = append(rs[i].Instructions, vm.Instruction{
-					Op:  overload.Join,
+					Op:  vm.Join,
 					Arg: constructJoin(n, c.proc),
 				})
 			}
 		}
-	case plan.Node_SEMI:
-		for i := range rs {
-			rs[i].Instructions = append(rs[i].Instructions, vm.Instruction{
-				Op:  overload.Semi,
-				Arg: constructSemi(n, c.proc),
-			})
-		}
-	case plan.Node_OUTER:
-		for i := range rs {
-			rs[i].Instructions = append(rs[i].Instructions, vm.Instruction{
-				Op:  overload.Left,
-				Arg: constructLeft(n, c.proc),
-			})
-		}
-	case plan.Node_ANTI:
-		for i := range rs {
-			rs[i].Instructions = append(rs[i].Instructions, vm.Instruction{
-				Op:  overload.Complement,
-				Arg: constructComplement(n, c.proc),
-			})
-		}
+
 	default:
-		panic(errors.New(errno.SyntaxErrororAccessRuleViolation, fmt.Sprintf("join typ '%v' not support now", n.JoinType)))
+		panic("not supported")
 	}
 	return rs
 }
--- pkg/vm/overload.go
+++ pkg/vm/overload.go
@@ -4,21 +4,20 @@ import (
 	"bytes"
 	"mo_join/pkg/sql/colexec/connector"
 	"mo_join/pkg/sql/colexec/join"
-	"mo_join/pkg/vm/overload"
 	"mo_join/pkg/vm/process"
 )
 
 var stringFunc = [...]func(interface{}, *bytes.Buffer){
-	overload.Join:      join.String,
-	overload.Connector: connector.String,
+	Join:      join.String,
+	Connector: connector.String,
 }
 
 var prepareFunc = [...]func(*process.Process, interface{}) error{
-	overload.Join:      join.Prepare,
-	overload.Connector: connector.Prepare,
+	Join:      join.Prepare,
+	Connector: connector.Prepare,
 }
 
 var execFunc = [...]func(*process.Process, interface{}) (bool, error){
-	overload.Join:      join.Call,
-	overload.Connector: connector.Call,
+	Join:      join.Call,
+	Connector: connector.Call,
 }
