cc699f044ba09e23609f8580ae7b6a2af2f79e1e
Arjun Sunil Kumar <arjunsk@users.noreply.github.com>
2024-04-17 16:17:16 -0700

    init

--- pkg/vm/mempool/mpool.go
+++ pkg/vm/mempool/mpool.go
@@ -16,22 +16,6 @@ func (m *Mempool) Size() int64 {
 	return int64(m.currSize)
 }
 
-func (m *Mempool) Alloc(size int) []byte {
-	m.currSize += size
-	size = ((size + PageSize - 1 + CountSize) >> PageOffset) << PageOffset
-	if size > m.maxSize {
-		panic("size too large")
-	}
-	data := make([]byte, size)
-	copy(data, OneCount)
-	return data
-}
-
-func (m *Mempool) Free(data []byte) bool {
-	m.currSize -= cap(data)
-	return true
-}
-
 func Alloc(m *Mempool, size int) (ret []byte) {
 	return make([]byte, size)
 }
--- pkg/vm/process/process.go
+++ pkg/vm/process/process.go
@@ -1,13 +1,27 @@
 package process
 
-import "mo_join/pkg/vm/mempool"
+import (
+	"context"
+	"mo_join/pkg/vm/mempool"
+	"mo_join/pkg/z/container/batch"
+)
+
+type Process struct {
+	Reg Register
+	Mp  *mempool.Mempool
+}
+type Register struct {
+	InputBatch     *batch.Batch
+	MergeReceivers []*WaitRegister
+}
+
+type WaitRegister struct {
+	Ctx context.Context
+	Ch  chan *batch.Batch
+}
 
 func New(mp *mempool.Mempool) *Process {
 	return &Process{
 		Mp: mp,
 	}
-}
-
-func (p *Process) Size() int64 {
-	return p.Mp.Size()
 }
